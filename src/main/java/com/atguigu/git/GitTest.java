bd9286f985eecf87fffe568996ade191f49ab9ad
root <1543037513@qq.com>
2022-07-27 08:25:10 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,20 @@
+package com.atguigu.git;
+
+/**
+ * @author LuJuncheng
+ * @version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("hello git2!");
+        System.out.println("hello git3!");
+        System.out.println("hello git4!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+        System.out.println("gitee test!");
+        System.out.println("gitee test2!");
+    }
+}
